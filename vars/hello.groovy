e880cf6c4d5bde67159f77314024964afb18a2fd
ryan.firmansyah <ryan.firmansyah@psn.co.id>
2023-01-03 17:45:12 +0700

    Adding new function post

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,3 +1,25 @@
 def world(){
     echo "Hello World"
 }
+
+def post(){
+    if (currentBuild.currentResult == 'SUCCESS') {
+        echo "Building Success"
+        slackSend (
+            channel: "#jenkins-notification", 
+            color: "good", 
+            message: 
+            "*${currentBuild.currentResult}:* Job #${env.BUILD_NUMBER} '${env.JOB_NAME}' on branch ${env.BRANCH_NAME}\
+            \n${env.BUILD_URL}"
+    )
+    } else if (currentBuild.currentResult == 'FAILURE') {
+        echo "Building Failed"
+        slackSend (
+            channel: "#jenkins-notification", 
+            color: "danger", 
+            message: 
+            "*${currentBuild.currentResult}:* Job #${env.BUILD_NUMBER} '${env.JOB_NAME}' on branch ${env.BRANCH_NAME}\
+            \n${env.BUILD_URL}"
+        )
+    }
+}
