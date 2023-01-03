f6290b3f5f6bc41eec1b857e60783d16de2b11ef
ryan.firmansyah <ryan.firmansyah@psn.co.id>
2023-01-03 19:04:03 +0700

    add maven pipeline

---
+++ vars/mavenPipeline.groovy
@@ -0,0 +1,53 @@
+def call(){
+    pipeline{
+        agent any
+        stages{
+            stage("Hello World from Jenkins shared library"){
+                steps{
+                    echo "Using jenkins Shared library"
+                    echo "on dev branch"
+                    script{
+                        hello.world()
+                    }
+                }
+            }
+            stage("Globall Variable"){
+                steps{
+                    script{
+                        echo author()
+                        echo author.name()
+                        echo author.asal()
+                    }
+                }
+            }
+        }
+            post{
+            always {
+                echo "Publish Notification build on Slack"
+                script {
+                    hello.post()
+                    // if (currentBuild.currentResult == 'SUCCESS') {
+                    //     echo "Building Success"
+                    //     slackSend (
+                    //         channel: "#jenkins-notification", 
+                    //         color: "good", 
+                    //         message: 
+                    //         "*${currentBuild.currentResult}:* Job #${env.BUILD_NUMBER} '${env.JOB_NAME}' on branch ${env.BRANCH_NAME}\
+                    //         \n${env.BUILD_URL}"
+                    //     )
+                    // } else if (currentBuild.currentResult == 'FAILURE') {
+                    //     echo "Building Failed"
+                    //     slackSend (
+                    //         channel: "#jenkins-notification", 
+                    //         color: "danger", 
+                    //         message: 
+                    //         "*${currentBuild.currentResult}:* Job #${env.BUILD_NUMBER} '${env.JOB_NAME}' on branch ${env.BRANCH_NAME}\
+                    //         \n${env.BUILD_URL}"
+                    //     )
+                    // }
+                }
+            }
+        }
+    }
+
+}
